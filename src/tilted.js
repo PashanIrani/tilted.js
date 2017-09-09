2ff600b19b2be98f4e6e725b8190dbf086ba44e3
Pashan Irani <pashan@Pashans-MBP-2.hitronhub.home>
2017-09-08 18:58:41 -0700

    a start

--- src/tilted.js
+++ src/tilted.js
@@ -1,6 +1,25 @@
+import './styles/main.scss';
 
-var a = function(tag_id, params) {
-  console.log('init');
+var rotate = function(event, el) {
+  var amount = 20;
+  var x = event.clientX;
+  var y = event.clientY;
+  var w = window.innerWidth;
+  var h = window.innerHeight;
+  var midpoint = w / 2;
+  var midpointY = h / 2;
+  var pos = x - midpoint;
+  var posY = y - midpointY;
+
+  var val = (pos / midpoint) * amount;
+  var valY = (posY / midpointY) * -amount;
+
+  el.style.transform = 'perspective(550px) rotateY(' + val + 'deg) rotateX(' + valY + 'deg)';
 };
 
-window.image = a;
+window.tilted = function(tag_id, params) {
+  var el = document.getElementById(tag_id);
+  document.addEventListener('mousemove', function(event) {
+    rotate(event, el);
+  }, false);
+};
--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,29 @@ module.exports = {
   output: {
     filename: 'tilted.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  watch: true,
+  module: {
+    rules: [{
+        test: /\.scss$/,
+        use: [{
+          loader: 'style-loader' // creates style nodes from JS strings
+        }, {
+          loader: 'css-loader' // translates CSS into CommonJS
+        }, {
+          loader: 'resolve-url-loader'
+        }, {
+          loader: 'sass-loader' // compiles Sass to CSS
+        }]
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        loader: 'file-loader?name=dist/fonts/[name].[ext]'
+      },
+      {
+        test: /\.(jpg|png)$/,
+        loader: 'file-loader?name=dist/fonts/[name].[ext]'
+      }
+    ]
   }
 };
