a2716d172a094e48a29bc2eecccce1d757d4a2d6
Pashan Irani <pashan@pashans-mbp-2.wifi.bcit.ca>
2017-09-13 17:23:16 -0700

    added uglify

--- src/tilted.js
+++ src/tilted.js
@@ -53,33 +53,29 @@ window.tilted = function(tag_id, params) {
   }
 
   if (onHover) {
-    el.addEventListener('mouseenter', function(event) {
-      var degs = calcTilt(event, el, intensity, disableX, disableY, invert);
-      rotate(el, degs.x, degs.y, function() {
-        el.addEventListener('mousemove', function(event) {
-          var degs = calcTilt(event, el, intensity, disableX, disableY, invert);
-          tilt(el, degs.x, degs.y);
-        }, false);
-      }, 100);
-    });
-
-    el.addEventListener('mouseleave', function() {
-        el.removeEventListener('mousemove', function(event) {
-          var degs = calcTilt(event, el, intensity, disableX, disableY, invert);
-          tilt(el, degs.x, degs.y);
-        }, false);
-        rotate(el, 0, 0);
-      });
-
+    el.addEventListener('mouseenter', start);
+    el.addEventListener('mouseleave', leave);
   } else {
-    document.addEventListener('mousemove', function(event) {
-      var degs = calcTilt(event, el, intensity, disableX, disableY, invert);
-      tilt(el, degs.x, degs.y);
-    }, false);
-
-    document.addEventListener('mouseleave', function(event) {
-      rotate(el, 0, 0);
-    }, false);
+    document.addEventListener('mouseenter', start);
+    document.addEventListener('mouseleave', leave);
   }
 
+  function start(event) {
+    var degs = calcTilt(event, el, intensity, disableX, disableY, invert);
+
+    //rotate to deg on enter then follow
+    rotate(el, degs.x, degs.y, function() {
+      el.addEventListener('mousemove', follow, false);
+    }, 100);
+  }
+
+  function follow(event) {
+    var degs = calcTilt(event, el, intensity, disableX, disableY, invert);
+    tilt(el, degs.x, degs.y);
+  }
+
+  function leave() {
+    el.removeEventListener('mousemove', follow, false);
+    rotate(el, 0, 0);
+  }
 };
