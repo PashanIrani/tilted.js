9cfac8bf419d406f1f0273409b40ca71fe5461e6
Pashan Irani <pashan@Pashans-MBP-2.hitronhub.home>
2017-09-18 20:36:53 -0700

    more tweaks

--- dist/tilted.js
+++ dist/tilted.js
@@ -95,12 +95,12 @@ var calcTilt = function(event, el, intensity, disableX, disableY, invert) {
   };
 };
 
-function rotate(el, x, y, callback, time) {
+function rotate(el, x, y, time, callback) {
 
   if (el == document) return;
   time = time ? time : 500;
   el.style.transition = time + 'ms';
-  el.style.transform = 'perspective(550px) rotateX(' + x + 'deg) rotateY(' + y + 'deg)  ';
+  el.style.transform = 'perspective(550px) rotateX(' + x + 'deg) rotateY(' + y + 'deg)';
   setTimeout(function() {
     el.style.transition = '';
     if (callback) {
@@ -116,6 +116,8 @@ function tilt(el, x, y) {
 
 window.tilted = function(tag_id, params) {
 
+  var state = 0;
+
   var el = document.getElementById(tag_id);
   if (params) {
     var intensity = params.intensity;
@@ -129,14 +131,19 @@ window.tilted = function(tag_id, params) {
 
   obj.addEventListener('mouseenter', start);
   obj.addEventListener('mouseleave', leave);
+  obj.addEventListener('mousemove', follow, false);
 
   function start(event) {
-    var degs = calcTilt(event, obj, intensity, disableX, disableY, invert);
-
-    //rotate to deg on enter then follow
-    rotate(el, degs.x, degs.y, function() {
-      obj.addEventListener('mousemove', follow, false);
-    }, 500);
+    if (state == 0) {
+      var degs = calcTilt(event, obj, intensity, disableX, disableY, invert);
+      state = 1;
+      console.log(state);
+      //rotate to deg on enter then follow
+      rotate(el, degs.x, degs.y, 100, function() {
+        state = 2;
+        console.log(state);
+      });
+    }
   }
 
   function follow(event) {
@@ -145,8 +152,11 @@ window.tilted = function(tag_id, params) {
   }
 
   function leave() {
-    obj.removeEventListener('mousemove', follow, false);
-    rotate(el, 0, 0);
+    state = 1;
+    rotate(el, 0, 0, 500, function() {
+      state = 0;
+      console.log(state);
+    });
   }
 };
 
--- src/tilted.js
+++ src/tilted.js
@@ -22,12 +22,12 @@ var calcTilt = function(event, el, intensity, disableX, disableY, invert) {
   };
 };
 
-function rotate(el, x, y, callback, time) {
+function rotate(el, x, y, time, callback) {
 
   if (el == document) return;
   time = time ? time : 500;
   el.style.transition = time + 'ms';
-  el.style.transform = 'perspective(550px) rotateX(' + x + 'deg) rotateY(' + y + 'deg)  ';
+  el.style.transform = 'perspective(550px) rotateX(' + x + 'deg) rotateY(' + y + 'deg)';
   setTimeout(function() {
     el.style.transition = '';
     if (callback) {
@@ -43,6 +43,8 @@ function tilt(el, x, y) {
 
 window.tilted = function(tag_id, params) {
 
+  var state = 0;
+
   var el = document.getElementById(tag_id);
   if (params) {
     var intensity = params.intensity;
@@ -56,14 +58,19 @@ window.tilted = function(tag_id, params) {
 
   obj.addEventListener('mouseenter', start);
   obj.addEventListener('mouseleave', leave);
+  obj.addEventListener('mousemove', follow, false);
 
   function start(event) {
-    var degs = calcTilt(event, obj, intensity, disableX, disableY, invert);
-
-    //rotate to deg on enter then follow
-    rotate(el, degs.x, degs.y, function() {
-      obj.addEventListener('mousemove', follow, false);
-    }, 500);
+    if (state == 0) {
+      var degs = calcTilt(event, obj, intensity, disableX, disableY, invert);
+      state = 1;
+      console.log(state);
+      //rotate to deg on enter then follow
+      rotate(el, degs.x, degs.y, 100, function() {
+        state = 2;
+        console.log(state);
+      });
+    }
   }
 
   function follow(event) {
@@ -72,7 +79,10 @@ window.tilted = function(tag_id, params) {
   }
 
   function leave() {
-    obj.removeEventListener('mousemove', follow, false);
-    rotate(el, 0, 0);
+    state = 1;
+    rotate(el, 0, 0, 500, function() {
+      state = 0;
+      console.log(state);
+    });
   }
 };
